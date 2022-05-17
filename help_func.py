40b6ba3a782e2a1f299c2edfb514a0c62562d6d4
Danny <danny-mhlv@gmail.com>
2022-05-18 02:15:13 +0300

    Initial commit. Init/Cross/Mutation added

---
+++ help_func.py
@@ -0,0 +1,54 @@
+import numpy as np
+import random
+
+
+def get_target(x):
+    return 0.2*x + x*np.sin(8.9*x) + 0.9*np.cos(1.5*x - 2)
+
+
+def get_fitness(x):
+    return (0.2*x + x*np.sin(8.9*x) + 0.9*np.cos(1.5*x - 2)) + 2.5
+
+
+def generate_chrom(length):
+    out = []
+    for i in range(length):
+        out.append(random.randint(0, 1))
+    return out
+
+
+def pop_random(lst):
+    index = random.randrange(0, len(lst))
+    return lst.pop(index)
+
+
+def make_pairs(population):
+    pairs = []
+    copy = population.copy()
+    while copy:
+        parent1 = pop_random(copy)
+        parent2 = pop_random(copy)
+        pair = parent1, parent2
+        pairs.append(pair)
+        # print(f"Parent1: {pair[0].genotype} | Parent2: {pair[1].genotype}")
+    return pairs
+
+
+def cross(pair, chrom_len, p_mutation):
+    genome1 = pair[0].genotype
+    genome2 = pair[1].genotype
+
+    cut_index = random.randint(0, chrom_len-1)  # Revert to chrom_len??
+    for index in range(cut_index, chrom_len):
+        tmp = genome1[index]
+        genome1[index] = genome2[index]
+        genome2[index] = tmp
+
+    if random.uniform(0, 1) <= p_mutation:
+        mutated_gene = random.randint(0, chrom_len)
+        genome1[mutated_gene] = 0 if genome1[mutated_gene] == 1 else 1
+    if random.uniform(0, 1) <= p_mutation:
+        mutated_gene = random.randint(0, chrom_len)
+        genome2[mutated_gene] = 0 if genome2[mutated_gene] == 1 else 1
+
+    return [genome1, genome2]
---
+++ main.py
@@ -0,0 +1,51 @@
+from matplotlib import pyplot as plt
+import numpy as np
+import help_func as myfunc
+import random
+
+POP_COUNT = 50
+CHROM_LEN = 20
+
+P_CROSS = 0.85
+P_MUTATION = 0.15
+RANGE_L = -3
+RANGE_R = 3
+
+step = (RANGE_R - RANGE_L) / 2 ** CHROM_LEN
+print(step)
+
+
+class Individual:
+    def __init__(self, phenotype, genotype, fitness) -> None:
+        self.phenotype = phenotype
+        self.genotype = genotype
+        self.fitness = fitness
+
+
+# Initialize
+population = []
+for i in range(POP_COUNT):
+    genotype = myfunc.generate_chrom(CHROM_LEN)
+    phenotype = random.uniform(-3, 3)
+    fitness = myfunc.get_fitness(phenotype)
+
+    population.append(Individual(phenotype, genotype, fitness))
+
+# Generate pairs
+pairs = myfunc.make_pairs(population)
+# Crossing and mutation
+for pair in pairs:
+    if random.uniform(0, 1) <= P_CROSS:
+        children = myfunc.cross(pair, CHROM_LEN, P_MUTATION)
+        population.extend(children)
+# Reduction (Roulette)
+
+# X = []
+# for i in range(POP_COUNT):
+#     X.append(population[i][0])
+# Y = []
+# for i in range(POP_COUNT):
+#     Y.append(population[i][1])
+
+# plt.plot(X, Y)
+# plt.show()
